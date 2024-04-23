c09a1244f9d012144a75c526a03c23774dc77d98
KG <c0b230680f@edu.teu.ac.jp>
2024-04-23 15:20:33 +0900

    練習４

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -43,6 +43,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bd_rct): #こうかとんと爆弾が接触してゲームオーバー
+            print("Game Over")
+            return
+        
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
